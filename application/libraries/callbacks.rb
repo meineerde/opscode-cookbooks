4f1da561fd0a115d63443eaeeaa2c8d2f420b91e
Holger Just <h.just@finn.de>
2011-09-18 16:29:42 +0200

    Allow multiple callbacks

--- application/libraries/callbacks.rb
+++ application/libraries/callbacks.rb
@@ -5,15 +5,22 @@ module Opscode
         @callbacks ||= Mash.new
         @callbacks[recipe] ||= Mash.new
         @callbacks[recipe][type] ||= Mash.new
-        @callbacks[recipe][type][app_id] = block
+        @callbacks[recipe][type][app_id] ||= []
+        @callbacks[recipe][type][app_id] << block
       end
 
       def self.callback(recipe, type, app_id, args)
-        cb = @callbacks[recipe][type][app_id] || @callbacks[recipe][type][:__default__] rescue nil
-        return if cb.nil?
+        callbacks = @callbacks[recipe][type][app_id]
+        callbacks = @callbacks[recipe][type][:__default__] if (callbacks.nil? || callbacks.empty?)
+        return if (callbacks.nil? || callbacks.empty?)
 
-        Chef::Log.info "Running #{type} callback in application::#{recipe} for #{app_id}"
-        cb.call(args)
+        Chef::Log.info "Running #{type} callbacks in application::#{recipe} for #{app_id}"
+        callbacks.each{ |cb| cb.call(args) }
+        nil
+      end
+
+      def self.callbacks(recipe, type, app_id = :__default__)
+        @callbacks[recipe][type][app_id] rescue []
       end
     end
   end
